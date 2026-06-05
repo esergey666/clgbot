3dd9552e8af7e842a282ae199c2ecef7abcf817f
spime <spime@example.com>
2026-06-06 00:44:22 +0300

    Reduce main label memory usage

--- bot/services/label_generator.py
+++ bot/services/label_generator.py
@@ -14,22 +14,29 @@ class LabelGenerator:
         numFont_path: str | Path,
         qr_template_path: str | Path,
         dpi: int = 300,
-        scale: int = 2,
+        scale: int = 1,
     ):
         self.scale = scale
         self.template = Image.open(template_path).convert("RGBA")
-        self.template = self.template.resize(
-            (self.template.width * scale, self.template.height * scale),
-            resample=Image.Resampling.LANCZOS,
-        )
+        if scale != 1:
+            self.template = self.template.resize(
+                (self.template.width * scale, self.template.height * scale),
+                resample=Image.Resampling.LANCZOS,
+            )
         self.font_path = font_path
         self.numFont_path = numFont_path
         self.dpi = dpi
         self.qr_generator = CustomQrGenerator(qr_template_path, dpi=dpi)
         self.draw = ImageDraw.Draw(self.template)
 
+    def _fit(self, value: int) -> int:
+        return max(1, round(value * self.scale / 2))
+
+    def _fit_pos(self, pos: tuple[int, int]) -> tuple[int, int]:
+        return (self._fit(pos[0]), self._fit(pos[1]))
+
     async def scale_pos(self, pos):
-        return (pos[0] * self.scale, pos[1] * self.scale)
+        return self._fit_pos(pos)
 
     async def save(self, filename):
         self.template.save(filename, dpi=(self.dpi, self.dpi))
@@ -146,32 +153,32 @@ class LabelGenerator:
     async def generate_label(self, art, color, size_tag, code, certilogo_code, certilogo_url):
         certilogo_datemark: str = f"{art}-{color}-{size_tag}-{code}-{certilogo_code}"
 
-        await self.draw_text((504, 1015), art, ImageFont.truetype(self.font_path, 210))
-        await self.draw_text((504, 1364), color, ImageFont.truetype(self.font_path, 310))
+        await self.draw_text(self._fit_pos((504, 1015)), art, ImageFont.truetype(self.font_path, self._fit(210)))
+        await self.draw_text(self._fit_pos((504, 1364)), color, ImageFont.truetype(self.font_path, self._fit(310)))
         await self.draw_text(
-            (960, 2000),
+            self._fit_pos((960, 2000)),
             size_tag,
-            ImageFont.truetype(self.font_path, 290),
+            ImageFont.truetype(self.font_path, self._fit(290)),
             fill="black",
             inner_stroke=True,
-            inner_stroke_width=2,
+            inner_stroke_width=self._fit(2),
             stroke_color="black"
         )
-        await self.draw_text((510, 2600), code, ImageFont.truetype(self.font_path, 200))
+        await self.draw_text(self._fit_pos((510, 2600)), code, ImageFont.truetype(self.font_path, self._fit(200)))
 
-        await self.add_datemark(certilogo_datemark, (600, 3000), 1200)
+        await self.add_datemark(certilogo_datemark, self._fit_pos((600, 3000)), self._fit(1200))
 
         await self.draw_spaced_text(
-            (719, 7387),
+            self._fit_pos((719, 7387)),
             certilogo_code,
-            ImageFont.truetype(self.numFont_path, 127),
-            spacing=6,
+            ImageFont.truetype(self.numFont_path, self._fit(127)),
+            spacing=self._fit(6),
             fill="black",
             inner_stroke=True,
-            inner_stroke_width=2,
+            inner_stroke_width=self._fit(2),
             stroke_color="white"
         )
-        await self.add_qr(certilogo_url, (650, 7950), 1100)
+        await self.add_qr(certilogo_url, self._fit_pos((650, 7950)), self._fit(1100))
 
         output = BytesIO()
         self.template.save(output, format="PNG", dpi=(self.dpi, self.dpi))
